54644868d8f245849473219d761dcecbfd9830f8
no-shin <irisinoirisino@gmail.com>
2024-07-04 18:57:06 +0900

    HoT: initial revision

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+print("Tossing a coin...")
+head=0
+tail=0
+for i in range(3):
+    print("Round "+str(i+1)+": ",end="")
+    num = random.random()
+    num*=100
+    num=num//10
+    if num%2 == 1:
+        print("Tail")
+        tail+=1
+    else:
+        print("Head")
+        head+=1
+
+print(f"Heads: {head},Tails: {tail}")
